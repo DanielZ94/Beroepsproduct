cef0a690b41a4b754fb50e1c2df1f3888740d377
18064728 <47145360+18064728@users.noreply.github.com>
2019-04-05 01:46:08 +0200

    Update MoveTest.java

--- MoveTest.java
+++ MoveTest.java
@@ -1,21 +1,18 @@
 import org.junit.Test;
-
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-
 import static org.junit.Assert.*;
 
 public class MoveTest {
-/**
-* simple test with a simplified level
-*/
+
+    /**
+     * simple JUnit test to test if the movement system is working correctly
+     */
+
     private int rows = 0; // the row in which the player is located
     private int columns = 0; // the column in which the player is located
-    private Point point = new Point();
-    //Playfield p = new Playfield();
-    protected  String[][] grid = { //level 1
-            {"p", "w", "g",    "g", "g", "g", "g", "g", "g", "g"},
+    private Point point = new Point(); // x and y location
+    private String[][] grid = { // simplified level
+            {"g", "w", "g",    "g", "g", "g", "g", "g", "g", "g"},
             {"g", "w", "w",    "w", "w", "g", "g", "g", "g", "g"},
             {"g", "g", "g", "k100", "g", "g", "g", "g", "g", "g"},
             {"w", "w", "w",    "w", "w", "g", "g", "g", "g", "g"},
@@ -28,78 +25,96 @@ public class MoveTest {
     };
 
     /**
-    * checks if left is a legal move
-    */
+     * checks if left is a legal move
+     */
+
     public void left() {
-        if (columns > 1 && grid[rows][columns-1].equals("g") || columns > 1 && grid[rows][columns-1].equals("k100") || columns > 1 && grid[rows][columns-1].equals("k200") ||
-            columns > 1 && grid[rows-1][columns].equals("k300")) {
+        if (columns <= 0|| grid[rows][columns-1].equals("w")) {
+            System.out.println("you can't go left here");
+        } else if (grid[rows][columns-1].equals("g") || grid[rows][columns-1].equals("k100")||
+                grid[rows][columns-1].equals("k200") || grid[rows][columns-1].equals("k300")||
+                Playfield.grid[rows][columns-1].equals("b100") && Playfield.keyValue == 100 ||
+                Playfield.grid[rows][columns-1].equals("b200") && Playfield.keyValue == 200 ||
+                Playfield.grid[rows][columns-1].equals("b300") && Playfield.keyValue == 300) {
             columns--;
             point.setLocation(columns, rows);
             System.out.println(point);
-        } else {
-            System.out.println("you can't go left");
         }
     }
 
     /**
-    * checks if right is a legal move
-    */
+     * checks if right is a legal move
+     */
+
     public void right() {
-        if (columns < 9 && grid[rows][columns+1].equals("g") || columns < 9 && grid[rows][columns+1].equals("k100") ||
-                columns < 9 && grid[rows][columns+1].equals("k200") || columns < 9 && grid[rows+1][columns].equals("k300")) {
-            columns++;
-            point.setLocation(columns, rows);
-            System.out.println(point);
-        } else {
-            System.out.println("you can't go rigth");
-        }
+        if (grid[rows][columns+1].equals("w") || columns >= 9) {
+            System.out.println("you can't go right here");
+        } else if (grid[rows][columns+1].equals("g") || grid[rows][columns+1].equals("k100")||
+                    grid[rows][columns+1].equals("k200") || grid[rows][columns+1].equals("k300")||
+                    Playfield.grid[rows][columns+1].equals("b100") && Playfield.keyValue == 100 ||
+                    Playfield.grid[rows][columns+1].equals("b200") && Playfield.keyValue == 200 ||
+                    Playfield.grid[rows][columns+1].equals("b300") && Playfield.keyValue == 300) {
+                columns++;
+                point.setLocation(columns, rows);
+                System.out.println(point);
+            }
     }
 
 
     /**
-    * checks if up is a legal move
-    */
+     * checks if up is a legal move
+     */
+
     public void up() {
-        if (rows > 1 && grid[rows-1][columns].equals("g") || rows > 1 && grid[rows-1][columns].equals("k100") ||
-                rows > 1 && grid[rows-1][columns].equals("k200") || rows > 1 && grid[rows-1][columns].equals("k300")) {
+        if (rows <= 0|| grid[rows-1][columns].equals("w")) {
+            System.out.println("you can't go up here");
+        } else if (grid[rows][columns-1].equals("g") || grid[rows][columns-1].equals("k100")||
+                grid[rows][columns-1].equals("k200") || grid[rows][columns-1].equals("k300")||
+                Playfield.grid[rows-1][columns].equals("b100") && Playfield.keyValue == 100 ||
+                Playfield.grid[rows-1][columns].equals("b200") && Playfield.keyValue == 200 ||
+                Playfield.grid[rows-1][columns].equals("b300") && Playfield.keyValue == 300) {
             rows--;
             point.setLocation(columns, rows);
             System.out.println(point);
-        } else {
-            System.out.println("you can't go up");
         }
     }
 
-   /**
-    * checks if down is a legal move
-    */
+    /**
+     * checks if down is a legal move
+     */
+
     public void down() {
-        if (rows > 9) {
-            System.out.println("down out of bounds");
-        }
-        if (rows < 9 && grid[rows+1][columns].equals("g") || rows < 9 && grid[rows+1][columns].equals("k100") ||
-                rows < 9 && grid[rows+1][columns].equals("k200") || grid[rows+1][columns].equals("k300")) {
+        if (grid[rows+1][columns].equals("w") || columns >= 9) {
+            System.out.println("you can't go down here");
+        } else if (grid[rows+1][columns].equals("g") || grid[rows+1][columns].equals("k100")||
+                grid[rows+1][columns].equals("k200") || grid[rows+1][columns].equals("k300")||
+                Playfield.grid[rows+1][columns].equals("b100") && Playfield.keyValue == 100 ||
+                Playfield.grid[rows+1][columns].equals("b200") && Playfield.keyValue == 200 ||
+                Playfield.grid[rows+1][columns].equals("b300") && Playfield.keyValue == 300) {
             rows++;
             point.setLocation(columns, rows);
             System.out.println(point);
-        } else {
-            System.out.println("you can't go down");
         }
     }
+
+    /**
+     * simulates movement,
+     * if the methods are working correctly, player will end at Point x=4, y=2
+     */
 
     @Test
     public void testGUI() {
         Point expected = new Point(4,2);
+        left();  // illegal move
+        up();    // illegal move
+        right(); // illegal move
         down();  // 0,1
-        up();    // 0,1 because up is illegal
-        left();  // 0,1 because left is illegal
         down();  // 0,2
-        down();  // 0,2 because down is illegal
+        down();  // illegal move
         right(); // 1,2
         right(); // 2,2
-        right(); // 3,2
+        right(); // 3,3
         right(); // 4,2
         assertEquals(expected, point); //test passes because point is expected
-
     }
 }
